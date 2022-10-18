072cd356bb0fda9b009cfb7a352e06ca2d5da280
Turani Narcis-Robert <turaninarcis@gmail.com>
2022-10-18 19:44:57 +0300

    Prima Problema

--- Problema_1/Program.cs
+++ Problema_1/Program.cs
@@ -10,7 +10,23 @@ namespace Problema_1
     {
         static void Main(string[] args)
         {
+            Ecuatie();
 
+        }
+
+
+        private static void Ecuatie()
+        {
+            int a, b;
+            Console.WriteLine("Introduceti 2 valori, a si b pentru care doriti sa creati o ecuatie de gradul I");
+            string[] numere = Console.ReadLine().Split(' ');
+            a = int.Parse(numere[0]);
+            b = int.Parse(numere[1]);
+            Console.WriteLine($"Ecuatia dumneavoastra este: {a}x + {b} ");
+            float rezultat = (float)-b / a;
+
+            Console.WriteLine($"Necunoscuta x are valoarea:  {-b}/{a}");
+            Console.WriteLine($"Sau valoarea: {rezultat}");
         }
     }
 }
