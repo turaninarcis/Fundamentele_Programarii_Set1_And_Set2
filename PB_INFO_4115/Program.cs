5b3394a7d11865942c40b4b06384b275b75cbe16
Turani Narcis-Robert <turaninarcis@gmail.com>
2022-10-18 19:16:20 +0300

    Add project files.

---
+++ PB_INFO_4115/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+
+namespace PB_INFO_4115
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N, M;
+            //StreamWriter output = File.CreateText(@"C:\output.txt");
+            StreamReader input = new StreamReader(@"E:\C_SHARP_POJECTS\PB_INFO_4115\input.txt");
+        }
+    }
+}
